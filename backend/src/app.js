96d2f2999d3b9f44aef70e13a36ec868fb531c4f
Misaina <misainarakotomanolo@yahoo.fr>
2024-06-08 21:07:43 +0300

    feat(backend): Point the route of authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const authRouter = require('./routes/authRoutes'); 
 
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
 
 module.exports = app;
