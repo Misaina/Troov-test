1bcfed672b2d999e06ed716d6724a8b45efe6d7f
Misaina <misainarakotomanolo@yahoo.fr>
2024-06-11 14:18:12 +0300

    feat: add logout

---
+++ frontend/components/modals/LogoutModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-modal ref="logoutModal" @ok="$emit('confirm-logout')">
+    <template #modal-title>
+      Confirmation de déconnexion
+    </template>
+    Êtes-vous sûr de vouloir vous déconnecter ?
+  </b-modal>
+</template>
+
+<script>
+export default {
+  methods: {
+    show() {
+      this.$refs.logoutModal.show();
+    }
+  }
+};
+</script>
--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="text-center mb-4 mt-5" v-if="isLoggedIn">
+    <b-button variant="warning" @click="confirmLogout" class="mb-5">Déconnexion</b-button>
     <b-button variant="success" @click="showAddModal" class="mb-5">Ajouter un objet</b-button>
     <ObjectList @edit-object="showEditModal"></ObjectList>
     <ObjectForm
@@ -7,20 +8,22 @@
       :object-to-edit="selectedObject"
       @object-saved="fetchObjects"
     ></ObjectForm>
+    <LogoutModal ref="logoutModal" @confirm-logout="logout"></LogoutModal>
   </div>
 </template>
-
 
 <script>
 import ObjectList from '../components/home/ObjectList.vue';
 import ObjectForm from '../components/home/ObjectForm.vue';
+import LogoutModal from '../components/modals/LogoutModal.vue';
 import { mapActions } from 'vuex';
 import { isTokenExpired } from '../utils/tokenUtils'
 
 export default {
   components: {
     ObjectList,
-    ObjectForm
+    ObjectForm,
+    LogoutModal
   },
   data() {
     return {
@@ -37,15 +40,22 @@ export default {
     showEditModal(object) {
       this.selectedObject = object;
       this.$bvModal.show('object-modal');
+    },
+    confirmLogout() {
+      this.$refs.logoutModal.show();
+    },
+    logout() {
+      this.$store.commit('auth/CLEAR_AUTH');
+      this.$router.push('/login');
     }
   },
   mounted() {
     if (isTokenExpired()) {
-      this.$store.commit('auth/CLEAR_AUTH');
-      return this.$router.push('/login');
+      return this.logout();
     }
     this.isLoggedIn = true;
     this.fetchObjects();
   }
 }
 </script>
+
